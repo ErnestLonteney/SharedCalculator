4430d848d4366f263c48c296734488a7b5cab6d3
Kseniia <kseniiadeumr@gmail.com>
2024-12-23 17:41:57 +0100

    Add Minus Command

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -194,8 +194,16 @@ namespace SharedCalculator
 
         Task AddMinusCommandExecute()
         {
-            
-            currentValue = "result here";
+
+            if (double.TryParse(currentValue, out double numValue))
+            {
+                numValue *= -1;
+                currentValue = numValue.ToString();
+            }
+            else
+            {
+                currentValue = "0";
+            }
 
             RaisePropertyChanged(nameof(CurrentValue));
 
