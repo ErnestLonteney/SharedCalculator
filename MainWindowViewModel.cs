1ef1ea434ce89878796f5048bb4dfc5baeddfa76
halyna.felinska <g.felinskaya@gmail.com>
2025-01-20 17:44:16 +0200

    dyscryminant method added

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -278,7 +278,55 @@ namespace SharedCalculator
         { 
             return a - b;
         }
+        {
+    class System
+    {
+        static void CalculateDescriminant (float a, float b, float c)
+        {
+            float descriminant = (float)Math.Pow(b,2) - 4 * a * c;
+            
+            Console.Write($"Дескримінант дорівнює: {descriminant}");
 
-        #endregion
+            float x1;
+            float x2;
+
+//Написати програму яка буде вираховувати квадратні корені рівняння. Нагадую що є три сценарія - коли Д <0 Д>0 і Д==0
+//Якщо дискримінант менше нуля (D < 0), рівняння не має дійсних коренів.
+            if (descriminant < '0')
+            {
+            Console.WriteLine(" рівняння не має дійсних коренів");
+            }
+//Якщо дискримінант дорівнює 0 (D = 0), рівняння має один дійсний корінь.
+           if (descriminant == '0')
+           {
+           Console.WriteLine(" рівняння має один дійсний корінь");
+           x1 = -b/(2*a);
+           Console.WriteLine($"корінь дорівнює:{x1}");
+           }
+
+//Якщо дискримінант більший за 0 (D > 0), рівняння має два різних дійсних коренів.
+           if (descriminant > '0')
+           {
+           Console.WriteLine(" рівняння має два різних дійсних корені.");
+           x1 = -b+(float)Math.Sqrt(descriminant)/(2*a);
+           x2 = -b-(float)Math.Sqrt(descriminant)/(2*a);
+           Console.WriteLine($"перший корінь дорівнює:{x1}, другий корінь дорівнює:{x2}");
+           }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Input a please");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Input b please");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Input c please");
+            float c = float.Parse(Console.ReadLine());
+
+            CalculateDescriminant (a, b, c);
+        }
+
     }
 }
+        #endregion
+    }
