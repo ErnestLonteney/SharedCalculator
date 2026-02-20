de3b9807fbb3193b7fc650fe5e4f5c2abfb73936
Olena <Lenkaeua@gmail.com>
2026-02-20 08:26:19 -0600

    Update ver

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -147,7 +147,7 @@ namespace SharedCalculator
         // Олена Мельник
         double Add(double value1, double value2)
         {
-            return 0;
+            return value1 + value2;
         }
 
         // Дарина Шевченко отримання відсотків
